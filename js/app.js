a4d283731c465a79d3fcde29b85b5719694f5f03
Fabian Pena <camilo.fabis@gmail.com>
2020-08-22 22:43:38 -0400

    First commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// Variables 
+const container = document.querySelector('.skills');
+const profilePic = document.querySelector('.profile-pic');
+const h2Headers = document.querySelector('.hireme');
+
+
+// Logos Function
+container.addEventListener('mouseover', function() {
+  // console.log('hihihih')
+  this.classList.add('shake-animation');
+})
+container.addEventListener('mouseleave', function() {
+  this.classList.remove('shake-animation');
+})
+
+// Proficle Pic Rotation Function
+profilePic.addEventListener('mouseover', function() {
+  // console.log('hello')
+  this.classList.add('hover');
+})
+
+// Header H2 functions
+h2Headers.addEventListener('mouseover', function() {
+  console.log("jgsega")
+  this.classList.add('zoom')
+})
