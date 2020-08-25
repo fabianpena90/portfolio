91b4923e99d86d921aad84338e3729f72cafa9e6
Fabian Pena <camilo.fabis@gmail.com>
2020-08-25 17:18:30 -0400

    fixed api link

--- js/jokes.js
+++ js/jokes.js
@@ -2,7 +2,7 @@ document.querySelector('.get-joke').onclick = getJoke
 
 
 function getJoke(){
-    axios.get('http://api.icndb.com/jokes/random/').then((res)=>{
+    axios.get('https://api.icndb.com/jokes/random/').then((res)=>{
           document.querySelector('.get-joke-list').innerHTML += `<li class="list">${res.data.value.joke}</li>`  
         })
 }
