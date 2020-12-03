27cc170971f790957a657f3aaec64c6bab1f5023
Fabian Pena <camilo.fabis@gmail.com>
2020-12-03 18:55:07 -0500

    redesign whole website added sass

--- js/app.js
+++ js/app.js
@@ -1,34 +1,5 @@
-//Welcome Message
-const btn = document.querySelector('.btn-portfolio');
-btn.addEventListener('click', clickMe);
+const btn = document.querySelector('.btn-work');
 
-function clickMe() {
-  const name = prompt("What's your name");
-  alert(`Welcome ${name} to my Portfolio`)
-}
-
-// Profile pic 
-const profilePic = document.querySelector('.profile-pic');
-profilePic.addEventListener('mouseover', profilePicture);
-
-function profilePicture(e) {
-  const pic = e.target;
-  if (pic !== 'pictureMe') {
-    pic.classList.toggle('pictureMe')
-  }
-}
-
-// Social Media Function
-const logos = document.querySelectorAll('.footerLogos');
-for (let i = 0; i < logos.length; i++) {
-  const skills = logos[i];
-  skills.onmouseover = logo;
-}
-
-function logo(e) {
-  const skills = e.target;
-  if (skills !== 'footer') {
-    skills.classList.toggle('footer')
-  }
-}
-
+btn.addEventListener('click', () => {
+  window.location.href = '#about'
+})
